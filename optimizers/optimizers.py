cb088f984b6f0522eb6ec95b0af9d161ee0ee4f0
Rasim <r_mamutov@mail.ru>
2021-11-21 17:52:38 +0700

    Updated optimizers

--- optimizers/optimizers.py
+++ optimizers/optimizers.py
@@ -10,17 +10,33 @@ from element_parser.data_parser import describe_elements, describe_correctors, m
 class GaussNewton:
     def __init__(self, structure, step, iteration=2):
         self.structure = structure
-        self.elements_to_vary, self.initial_parameters = describe_elements(self.structure.structure, "madx\elements\combined_magnets.txt")
+        self.elements_to_vary, self.initial_parameters = describe_elements(self.structure.structure,
+                                                                           "madx\elements\combined_magnets.txt")
         self.correctors, _ = describe_correctors(self.structure.structure, "madx\correctors\correctors.txt")
         self.bad_correctors, _ = describe_correctors(self.structure.bad_structure, "madx\correctors\correctors.txt")
-        self.bad_elements_to_vary, self.bad_initial_parameters = describe_elements(self.structure.bad_structure, "madx\elements\combined_magnets.txt")
+        self.bad_elements_to_vary, self.bad_initial_parameters = describe_elements(self.structure.bad_structure,
+                                                                                   "madx\elements\combined_magnets.txt")
         self.bad_elements_to_vary, self.bad_initial_parameters = match_elements_indices_of_two_structures(
             self.elements_to_vary, self.initial_parameters, self.bad_elements_to_vary, self.bad_initial_parameters)
         self.elements_number = len(self.elements_to_vary)
-        self.shape = [22, self.elements_number]
-        # self.shape = [22 * 108, self.elements_number]
+        # self.shape = [22, self.elements_number]
+        self.shape = [22 * 108, self.elements_number]
         self.step = step
         self.iteration = iteration
+
+    def _get_residual(self, bad_response_matrix, model_response_matrix, weights=None, vector_type="Flatenned"):
+        if vector_type == "axis-0 summed":
+            vector = np.sum(bad_response_matrix - model_response_matrix, 0)
+        elif vector_type == "axis-1 summed":
+            vector = np.sum(bad_response_matrix - model_response_matrix, 1)
+        else:
+            vector = (bad_response_matrix - model_response_matrix).to_numpy().flatten()
+
+        if isinstance(weights, (list, np.ndarray)):
+            vector = np.concatenate((vector, weights))
+        residual = np.sum(vector * vector)
+
+        return vector, residual
 
     def optimize_lattice(self):
         """
@@ -39,10 +55,7 @@ class GaussNewton:
                                                                          self.elements_to_vary,
                                                                          accumulative_param_additive,
                                                                          self.correctors)
-        # initial_vector = np.sum(bad_response_matrix-model_response_matrix, 1)
-        initial_vector = np.sum(bad_response_matrix - model_response_matrix, 0)
-        # initial_vector = (bad_response_matrix - model_response_matrix).to_numpy().flatten()
-        initial_residual = np.sum(initial_vector*initial_vector)
+        initial_vector, initial_residual = self._get_residual(bad_response_matrix, model_response_matrix)
 
         count = 1
         while count <= self.iteration:
@@ -50,9 +63,7 @@ class GaussNewton:
                                                                                self.elements_to_vary,
                                                                                accumulative_param_additive,
                                                                                self.correctors)
-            # vector_1 = np.sum(bad_response_matrix-model_response_matrix_1, 1)
-            vector_1 = np.sum(bad_response_matrix - model_response_matrix_1, 0)
-            # vector_1 = (bad_response_matrix - model_response_matrix_1).to_numpy().flatten()
+            vector_1, _ = self._get_residual(bad_response_matrix, model_response_matrix_1)
 
             J = self.calculate_jacobian(accumulative_param_additive, model_response_matrix_1)
             u, sv, v = self.drop_bad_singular_values(J)
@@ -64,14 +75,11 @@ class GaussNewton:
                                                                                     self.elements_to_vary,
                                                                                     accumulative_param_additive,
                                                                                     self.correctors)
-            # final_vector = np.sum(bad_response_matrix-fitted_model_response_matrix, 1)
-            final_vector = np.sum(bad_response_matrix - fitted_model_response_matrix, 0)
-            # final_vector = (bad_response_matrix - fitted_model_response_matrix).to_numpy().flatten()
-            final_residual = np.sum(final_vector*final_vector)
+            final_vector, final_residual = self._get_residual(bad_response_matrix, fitted_model_response_matrix)
 
             print("Initial parameters: ", self.initial_parameters)
             print("Bad parameters: ", self.bad_initial_parameters)
-            print("Final parameters: ", list(self.initial_parameters+accumulative_param_additive))
+            print("Final parameters: ", list(self.initial_parameters + accumulative_param_additive))
             print("Initial residual: ", initial_residual)
             print("Final residual: ", final_residual)
 
@@ -90,31 +98,33 @@ class GaussNewton:
                                                                                self.elements_to_vary,
                                                                                accumulative_param_variation,
                                                                                self.correctors)
-            # vector_2 = np.sum(model_response_matrix_1-model_response_matrix_2, 1)
-            vector_2 = np.sum(model_response_matrix_1 - model_response_matrix_2, 0)
-            # vector_2 = (model_response_matrix_1 - model_response_matrix_2).to_numpy().flatten()
-            J[:,i] = vector_2 / self.step
-            print(datetime.now()-now)
+            vector_2, _ = self._get_residual(model_response_matrix_1, model_response_matrix_2)
+
+            J[:, i] = vector_2 / self.step
+            print(datetime.now() - now)
 
         return J
 
     def drop_bad_singular_values(self, J):
-        svd = np.linalg.svd(np.matmul(J.T,J), full_matrices=False)
+        svd = np.linalg.svd(np.matmul(J.T, J), full_matrices=False)
         u, sv, v = svd
         print("Singulars: ", sv)
-        # plt.plot(sv,marker='o')
-        # plt.show()
+
+        plt.plot(sv, marker='o', markersize=5, label="Singular values of J")
+        plt.legend()
+        plt.show()
+
         sv = np.linalg.pinv(np.diag(sv))
         for i in range(len(sv)):
-            if sv[i,i] > np.inf:
-                sv[i,i] = 0
+            if sv[i, i] > np.inf:
+                sv[i, i] = 0
 
         return u, sv, v
 
     def calculate_parameters_delta(self, J, u, sv, v, vector_1):
-        J_new = np.matmul(np.matmul(v.T,sv),u.T)
+        J_new = np.matmul(np.matmul(v.T, sv), u.T)
         # delta = -np.matmul(np.linalg.pinv(np.matmul(J.T,J)),J.T).dot(vector_1)
-        delta = -np.matmul(J_new,J.T).dot(vector_1)
+        delta = -np.matmul(J_new, J.T).dot(vector_1)
         # delta = np.where(np.abs(delta)>1e-3, 0.5*delta, delta) # To remove twiss instabillity
         print("delta", delta)
 
@@ -127,14 +137,15 @@ class GaussNewton:
 
         quadrupole_alignments = {}
         for i, quadrupole in enumerate(self.elements_to_vary):
-            response_matrix, response_matrix_at_beginning = self.structure.calculate_response_matrix_on_quadrupole_variation(self.structure.structure,
-                                                                                                                             quadrupole,
-                                                                                                                             1e-5,
-                                                                                                                             i)
+            response_matrix, response_matrix_at_beginning = self.structure.calculate_response_matrix_on_quadrupole_variation(
+                self.structure.structure,
+                quadrupole,
+                1e-5,
+                i)
             delta = np.linalg.pinv(response_matrix).dot(experimental_responses[quadrupole[0]])
             quadrupole_alignments[quadrupole[0]] = delta
             print('Quadrupole alignment:', str(i) + '/' + str(self.elements_number))
-            print('asdd',response_matrix_at_beginning)
+            print('asdd', response_matrix_at_beginning)
             x = response_matrix_at_beginning.dot(delta)
             print("final", x)
 
@@ -144,18 +155,28 @@ class GaussNewton:
 
 
 class LevenbergMarquardt(GaussNewton):
-    def __init__(self, structure, step, iteration=1, coefficient_lambda=0.0001):
+    def __init__(self, structure, step, iteration=1, coefficient_lambda=0.001):
         super().__init__(structure, step, iteration)
         self.coefficient_lambda = coefficient_lambda
 
     def drop_bad_singular_values(self, J):
-        svd = np.linalg.svd(np.matmul(J.T,J)+self.coefficient_lambda*np.diag(np.matmul(J.T,J)), full_matrices=False)
+        svd = np.linalg.svd(np.matmul(J.T, J) + self.coefficient_lambda * np.diag(np.diag(np.matmul(J.T, J))),
+                            full_matrices=False)
         u, sv, v = svd
         print("Singulars: ", sv)
+
+        plt.plot(sv,marker='o', markersize=5, label="Modified Jacobian")
+        svd_1 = np.linalg.svd(np.matmul(J.T, J), full_matrices=False)
+        _, sv_1, _ = svd_1
+        plt.plot(sv_1, color='r', marker='o', markersize=5, label="Not modified Jacobian")
+        plt.legend()
+        print("Singulars delta", sv-sv_1)
+        plt.show()
+
         sv = np.linalg.pinv(np.diag(sv))
         for i in range(len(sv)):
-            if sv[i,i] > np.inf:
-                sv[i,i] = 0
+            if sv[i, i] > np.inf:
+                sv[i, i] = 0
 
         return u, sv, v
 
@@ -163,7 +184,8 @@ class LevenbergMarquardt(GaussNewton):
 class GaussNewtonConstrained(GaussNewton):
     def __init__(self, structure, step, iteration=1):
         super().__init__(structure, step, iteration)
-        self.weights = np.ones(self.elements_number)*1e-3
+        self.weights = np.ones(self.elements_number) * 1e-3
+        self.weights = np.random.rand(self.elements_number) * 1e-4
 
     def optimize_lattice(self):
         """
@@ -182,11 +204,8 @@ class GaussNewtonConstrained(GaussNewton):
                                                                          self.elements_to_vary,
                                                                          accumulative_param_additive,
                                                                          self.correctors)
-        # initial_vector = np.sum(bad_response_matrix-model_response_matrix, 1)
-        # initial_vector = np.sum(bad_response_matrix - model_response_matrix, 0)
-        initial_vector = (bad_response_matrix - model_response_matrix).to_numpy().flatten()
-        initial_vector = np.concatenate((initial_vector, self.weights*delta))
-        initial_residual = np.sum(initial_vector * initial_vector)
+        initial_vector, initial_residual = self._get_residual(bad_response_matrix, model_response_matrix,
+                                                              self.weights * delta)
 
         count = 1
         while count <= self.iteration:
@@ -194,15 +213,12 @@ class GaussNewtonConstrained(GaussNewton):
                                                                                self.elements_to_vary,
                                                                                accumulative_param_additive,
                                                                                self.correctors)
-            # vector_1 = np.sum(bad_response_matrix-model_response_matrix_1, 1)
-            # vector_1 = np.sum(bad_response_matrix - model_response_matrix_1, 0)
-            vector_1 = (bad_response_matrix - model_response_matrix_1).to_numpy().flatten()
-            vector_1 = np.concatenate((vector_1, self.weights * delta))
+            vector_1, _ = self._get_residual(bad_response_matrix, model_response_matrix_1,
+                                                                  self.weights * delta)
 
             J = self.calculate_jacobian(accumulative_param_additive, model_response_matrix_1)
-            J = self.create_modified_jacobian(J, delta)
-            u, sv, v = self.drop_bad_singular_values(J)
-            delta = self.calculate_parameters_delta(J, u, sv, v, vector_1)
+            u, sv, v = self.drop_bad_singular_values(J, delta)
+            delta = self.calculate_parameters_delta(J_modified, u, sv, v, vector_1)
             accumulative_param_additive += delta
             count += 1
 
@@ -210,11 +226,8 @@ class GaussNewtonConstrained(GaussNewton):
                                                                                     self.elements_to_vary,
                                                                                     accumulative_param_additive,
                                                                                     self.correctors)
-            # final_vector = np.sum(bad_response_matrix-fitted_model_response_matrix, 1)
-            # final_vector = np.sum(bad_response_matrix - fitted_model_response_matrix, 0)
-            final_vector = (bad_response_matrix - fitted_model_response_matrix).to_numpy().flatten()
-            final_vector = np.concatenate((final_vector, self.weights * delta))
-            final_residual = np.sum(final_vector * final_vector)
+            final_vector, final_residual = self._get_residual(bad_response_matrix, fitted_model_response_matrix,
+                                                              self.weights * delta)
 
             print("Initial parameters: ", self.initial_parameters)
             print("Bad parameters: ", self.bad_initial_parameters)
@@ -227,3 +240,24 @@ class GaussNewtonConstrained(GaussNewton):
     def create_modified_jacobian(self, J, delta_qrad):
         W = np.diag(self.weights * delta_qrad)
         return np.concatenate((J, W), axis=0)
+
+    def drop_bad_singular_values(self, J, delta_qrad):
+        J_modified = self.create_modified_jacobian(J, delta_qrad)
+        svd = np.linalg.svd(np.matmul(J_modified.T, J_modified), full_matrices=False)
+        u, sv, v = svd
+        print("Singulars: ", sv)
+
+        plt.plot(sv,marker='o', markersize=5, label="Modified Jacobian")
+        svd_1 = np.linalg.svd(np.matmul(J.T, J), full_matrices=False)
+        _, sv_1, _ = svd_1
+        plt.plot(sv_1, color='r', marker='o', markersize=5, label="Not modified Jacobian")
+        plt.legend()
+        print("Singulars delta", sv-sv_1)
+        plt.show()
+
+        sv = np.linalg.pinv(np.diag(sv))
+        for i in range(len(sv)):
+            if sv[i, i] > np.inf:
+                sv[i, i] = 0
+
+        return u, sv, v
